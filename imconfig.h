a3a75ee20bc6508a478285cf9f8dcfbaa6fbd5f7
Robert Millar <robert.millar@cantab.net>
2017-08-11 22:32:11 -0700

    Local integration changes to imconfig

--- imconfig.h
+++ imconfig.h
@@ -6,6 +6,8 @@
 
 #pragma once
 
+#include "../Source//Vectors.h" // TODO
+
 //---- Define assertion handler. Defaults to calling assert().
 //#define IM_ASSERT(_EXPR)  MyAssert(_EXPR)
 
@@ -33,15 +35,14 @@
 //#define IMGUI_STB_NAMESPACE     ImGuiStb
 
 //---- Define constructor and implicit cast operators to convert back<>forth from your math types and ImVec2/ImVec4.
-/*
 #define IM_VEC2_CLASS_EXTRA                                                 \
-        ImVec2(const MyVec2& f) { x = f.x; y = f.y; }                       \
-        operator MyVec2() const { return MyVec2(x,y); }
+        ImVec2(const Vec2& f) { x = f.X; y = f.Y; }                       \
+        operator Vec2() const { return Vec2(x,y); }
 
 #define IM_VEC4_CLASS_EXTRA                                                 \
-        ImVec4(const MyVec4& f) { x = f.x; y = f.y; z = f.z; w = f.w; }     \
-        operator MyVec4() const { return MyVec4(x,y,z,w); }
-*/
+        ImVec4(const Vec4& f) { x = f.X; y = f.Y; z = f.Z; w = f.W; }     \
+        operator Vec4() const { return Vec4(x,y,z,w); }
+
 
 //---- Tip: You can add extra functions within the ImGui:: namespace, here or in your own headers files.
 //---- e.g. create variants of the ImGui::Value() helper for your low-level math types, or your own widgets/helpers.
